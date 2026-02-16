cc9940b405f83800162814d22e65d71fb513896c
jahyeonjo <amirgeraldgilbuena@gmail.com>
2026-02-16 20:32:35 +0800

    Refactor main.cpp to initialize LED pins and implement blinking sequence

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,49 @@
 
 // put function declarations here:
 int myFunction(int, int);
+int led0 = 8;
+int led1 = 9;
+int led2 = 10;
+int led3 = 11;
+int led4 = 12;
+int led5 = 13;
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(led0, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(led0, HIGH);
+  delay(1000);
+  digitalWrite(led0, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led3, LOW);
+  delay(1000);
+  digitalWrite(led4, HIGH);
+  delay(1000);
+  digitalWrite(led4, LOW);
+  delay(1000);
+  digitalWrite(led5, HIGH);
+  delay(1000);
+  digitalWrite(led5, LOW);
+  delay(1000);
 }
