7dcff75784bd8d88e2c0b53e6711a6674f473bc1
jahyeonjo <amirgeraldgilbuena@gmail.com>
2026-02-17 23:45:57 +0800

    refactor main.cpp to implement automatic led blinking sequence

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,67 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-int led0 = 8;
-int led1 = 9;
-int led2 = 10;
-int led3 = 11;
-int led4 = 12;
-int led5 = 13;
-
+// 7-segment display pin definitions
+const int SEGMENT_A = 2;
+const int SEGMENT_B = 3;
+const int SEGMENT_C = 4;
+const int SEGMENT_D = 5;
+const int SEGMENT_E = 6;
+const int SEGMENT_F = 8;
+const int SEGMENT_G = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(led0, OUTPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
+  // Initialize all segment pins as outputs
+  pinMode(SEGMENT_A, OUTPUT);
+  pinMode(SEGMENT_B, OUTPUT);
+  pinMode(SEGMENT_C, OUTPUT);
+  pinMode(SEGMENT_D, OUTPUT);
+  pinMode(SEGMENT_E, OUTPUT);
+  pinMode(SEGMENT_F, OUTPUT);
+  pinMode(SEGMENT_G, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led0, HIGH);
-  delay(1000);
-  digitalWrite(led0, LOW);
-  delay(1000);
-  digitalWrite(led1, HIGH);
-  delay(1000);
-  digitalWrite(led1, LOW);
-  delay(1000);
-  digitalWrite(led2, HIGH);
-  delay(1000);
-  digitalWrite(led2, LOW);
-  delay(1000);
-  digitalWrite(led3, HIGH);
-  delay(1000);
-  digitalWrite(led3, LOW);
-  delay(1000);
-  digitalWrite(led4, HIGH);
-  delay(1000);
-  digitalWrite(led4, LOW);
-  delay(1000);
-  digitalWrite(led5, HIGH);
-  delay(1000);
-  digitalWrite(led5, LOW);
-  delay(1000);
+void DisplayLetter(int letter){
+  // A
+  if(letter != 1 && letter != 2 && letter != 4 && letter != 7 && letter != 9)
+  digitalWrite(SEGMENT_A, LOW);
+  
+  // B
+  if(letter != 2 && letter != 5 && letter != 9)
+  digitalWrite(SEGMENT_B, LOW);
+  
+  // C
+  if(letter != 2)
+  digitalWrite(SEGMENT_C, LOW);
+  
+  // D
+  if(letter != 1 && letter != 2 && letter != 5 && letter != 7 && letter != 8)
+  digitalWrite(SEGMENT_D, LOW);
+  
+  if(letter == 0 || letter == 2 || letter == 3 || letter == 4 || letter == 5 || letter == 6 || letter == 7 || letter == 9)
+  digitalWrite(SEGMENT_E, LOW);
+  
+  if(letter != 1 && letter != 2 && letter != 7 && letter != 8)
+  digitalWrite(SEGMENT_F, LOW);
+  
+  if(letter != 0 && letter != 8 && letter != 9)
+  digitalWrite(SEGMENT_G, LOW);
+}
+
+void TurnOff(){
+  digitalWrite(SEGMENT_A, HIGH);
+  digitalWrite(SEGMENT_B, HIGH);
+  digitalWrite(SEGMENT_C, HIGH);
+  digitalWrite(SEGMENT_D, HIGH);
+  digitalWrite(SEGMENT_E, HIGH);
+  digitalWrite(SEGMENT_F, HIGH);
+  digitalWrite(SEGMENT_G, HIGH);
+}
+
+void loop()
+{
+  for(int i=0; i<10; i++){
+    DisplayLetter(i);
+    delay(1000);
+    TurnOff();
+  }
 }
